816b08fc0ca7c87ebe5b52209336e13465b187dc
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2020-09-23 23:17:55 +0530

    solution for 124

---
+++ flaskapp.py
@@ -0,0 +1,52 @@
+from flask import Flask,jsonify, request
+
+app = Flask(__name__)
+
+List = [
+    {
+        'id': 1,
+        'Name': u'Raju',
+        'Contact': u'9987644456', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'Name': u'Rahul',
+        'Contact': u'9876543222', 
+        'done': False
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/add-data", methods=["POST"])
+def add_task():
+    if not request.json:
+        return jsonify({
+            "status":"error",
+            "message": "Please provide the data!"
+        },400)
+
+    contact = {
+        'id': tasks[-1]['id'] + 1,
+        'Name': request.json['Name'],
+        'Contact': request.json.get('Contact', ""),
+        'done': False
+    }
+    List.append(contact)
+    return jsonify({
+        "status":"success",
+        "message": "Contact added succesfully!"
+    })
+    
+
+@app.route("/get-data")
+def get_task():
+    return jsonify({
+        "data" : List
+    }) 
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
